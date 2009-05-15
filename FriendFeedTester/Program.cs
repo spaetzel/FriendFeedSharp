8c6f33b453a4ba0ec254f8877dc2723bfb85dc94
spaetzel <william@spaetzel.com>
2009-05-15 14:50:13 -0400

    Expand tests

--- FriendFeedTester/Program.cs
+++ FriendFeedTester/Program.cs
@@ -17,12 +17,24 @@ namespace FriendFeedTester
 
             UserProfile profile = client.FetchUserProfile("ffsharp");
 
-            Console.WriteLine(profile.Lists.Count());
+            Console.WriteLine(String.Format("This user's id is {0}, they have created {1} user lists", profile.Id, profile.Lists.Count() ));
 
-
+            // Get the profile for a list
             ListProfile listProfile = client.FetchListProfile(profile.Lists.First().Nickname);
 
-            Console.WriteLine(listProfile.Users.Count());
+            Console.WriteLine(String.Format("The list, {0} has {1} users", listProfile.Name,  listProfile.Users.Count()));
+
+            User firstUser = listProfile.Users.First();
+
+            // Get the recent posts from a list member
+
+            Feed feed = client.FetchUserFeed( firstUser.Nickname );
+
+            Entry firstEntry = feed.First();
+
+            Console.WriteLine(String.Format("The most recent post from {0} has a ttile of \"{1}\" and an id of {2}", firstUser.Nickname, firstEntry.Title, firstEntry.Id ) );
+
+            Console.ReadLine();
         }
     }
 }
